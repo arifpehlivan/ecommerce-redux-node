f92132508de1df8f2c6dbb43f91879cd637f2834
arifpehlivan <arifpehlivanapap@gmail.com>
2022-12-14 19:10:02 +0300

    add navbar

---
+++ client/src/components/navbar/Navbar.js
@@ -0,0 +1,26 @@
+import './navbar.scss'
+import { Link } from "react-router-dom";
+// import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import AddShoppingCartRoundedIcon from '@mui/icons-material/AddShoppingCartRounded';
+
+const Navbar = () => {
+    return (
+        <div className='navbar'>
+            <div className="logo">
+                <Link to="/">
+                    Redux Shopping
+                </Link>
+            </div>
+            <div className="rightSide">
+                <div className="shoppingCart">
+                    <AddShoppingCartRoundedIcon style={{ cursor: "pointer" }} />
+                </div>
+                <div className="buttons">
+                    <Link to="/admin">Admin</Link>
+                </div>
+            </div>
+        </div >
+    )
+}
+
+export default Navbar
